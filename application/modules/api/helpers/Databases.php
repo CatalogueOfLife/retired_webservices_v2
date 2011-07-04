e03e724bc958a3b5821c86a4304270caadacbfba
Ayco Holleman <ayco.holleman@naturalis.nl>
2011-07-04 12:11:41 +0000

    SERV-19

--- application/modules/api/helpers/Databases.php
+++ application/modules/api/helpers/Databases.php
@@ -1,6 +1,6 @@
 <?php
 
-$default_database = "ayco-local";
+$default_database = "dev.4d4life.eu";
 
 $databases['2011']['adapter'] = "Mysqli";
 $databases['2011']['host'] = "localhost";
@@ -14,4 +14,12 @@ $databases['ayco-local']['host'] = "localhost";
 $databases['ayco-local']['dbname'] = "base_schema_v19";
 $databases['ayco-local']['username'] = "root";
 $databases['ayco-local']['password'] = "";
+$databases['ayco-local']['charset'] = "utf8";
+
+$databases['dev.4d4life.eu']['adapter'] = "pdo_mysql";
+$databases['dev.4d4life.eu']['host'] = "localhost";
+$databases['dev.4d4life.eu']['dbname'] = "bs_v19";
+$databases['dev.4d4life.eu']['username'] = "root";
+$databases['dev.4d4life.eu']['password'] = "poUss4ka";
+$databases['dev.4d4life.eu']['charset'] = "utf8";
 
