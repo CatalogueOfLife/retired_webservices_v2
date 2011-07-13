f77524cb2b41b380666edfba4d5ed2e5e6575a8a
Ayco Holleman <ayco.holleman@naturalis.nl>
2011-07-13 12:07:43 +0000

    SERV-19

--- application/modules/api/controllers/SourceDatabaseController.php
+++ application/modules/api/controllers/SourceDatabaseController.php
@@ -26,6 +26,59 @@ class SourceDatabaseController extends RestController
     
     }
 
+    public function flushAction ()
+    {
+        $flushTime = time();
+        $response = new api_classes_Response();
+        $objs = Zend_Json::decode($this->_param('flush'));
+        try {
+            foreach ($objs as $obj) {
+                $arr = array();
+                foreach ($obj as $key => $val) {
+                    $arr[$key] = $val;
+                    $sdb = new api_models_dao_SourceDatabase();
+                    $sdb->initialize($arr);
+                    $sdb->setFLushTime($flushTime);
+                    $sdb->save();
+                }
+            }
+        }
+        catch (Exception $e) {
+            $sdb = new api_models_dao_SourceDatabase();
+            $sdb->setFLushTime($flushTime);
+            $sdb->deleteByKey('flushTime');
+            $response->errLevel = api_classes_Response::ERR_LEVEL_ERROR;
+            $response->errMessage = get_class($e) . '. ' . $e->getMessage();
+            if ($this->_param('_debug') === 'true') {
+                $response->method = __METHOD__;
+                $response->params = $this->_request->getParams();
+                $response->stackTrace = $e->getTrace();
+            }
+        }
+        $this->_response->setHeader('Content-Type', 'application/json');
+        $this->_response->setBody(Zend_Json::encode($response));
+        $this->_response->sendResponse();
+        exit();
+    }
+
+    /**
+     * Validates the taxonomic coverage as entered manually in the Metadatabase.
+     * 
+     * Example:
+     * 
+     * http://localhost/webservices_v2/sourcedatabase/validate-taxonomic-coverage/? ... (to be specified)
+     * 
+     */
+    public function validateTaxonomicCoverageAction ()
+    {
+        $response = new api_classes_Response();
+        $response->returnValue = 1;
+        $this->_response->setHeader('Content-Type', 'application/json');
+        $this->_response->setBody(Zend_Json::encode($response));
+        $this->_response->sendResponse();
+        exit();
+    }
+
     /**
      * The following properties can be set and saved:
      * 
@@ -38,6 +91,7 @@ class SourceDatabaseController extends RestController
      * - version
      * - releaseDate
      * - abstract
+     * - taxonomicCoverage
      * - coverage
      * - completeness
      * - confidence
@@ -45,6 +99,8 @@ class SourceDatabaseController extends RestController
      * Example:
      * 
      * http://localhost/webservices_v2/sourcedatabase/save/?name=ayco&abbreviatedName=ayco&organisation=ETI
+     * 
+     * The "returnValue" property in the JSON response object contains the id of the new source database.
      * 
      */
     public function saveAction ()
