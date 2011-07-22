11e07f53acf095673e92d0f8518cd6ac2ccfba3f
Dennis Seijts <naturalis@users.noreply.github.com>
2011-07-22 07:45:44 +0000

    SERV-27

--- public/index.php
+++ public/index.php
@@ -15,7 +15,23 @@ try {
         APPLICATION_PATH . '/modules', 
         APPLICATION_PATH . '/classes', 
     )));
-        
+
+    //Detect magic quotes and remove slashes from cookies and requests
+    if(get_magic_quotes_gpc()) {
+	    $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+	    while (list($key, $val) = each($process)) {
+	        foreach ($val as $k => $v) {
+	            unset($process[$key][$k]);
+	            if (is_array($v)) {
+	                $process[$key][stripslashes($k)] = $v;
+	                $process[] = &$process[$key][stripslashes($k)];
+	            } else {
+	                $process[$key][stripslashes($k)] = stripslashes($v);
+	            }
+	        }
+	    }
+	    unset($process);
+    }
     
     require_once 'Zend/Application.php';
 /*     
